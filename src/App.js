f064e40d18993224191d5d69fb84076dfa55f783
Kimfucious <kimfucious@abts.io>
2020-01-07 23:05:58 +0700

    add Answer, Footer, and Show Answers

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import { Button, Collapse } from "reactstrap";
 import { problems } from "./problems";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { okaidia } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { Footer } from "./components/Footer";
+import { Answer } from "./components/Answer";
 
 function App() {
   const [isOpen, setIsOpen] = useState("");
+  const [showAnswer, setShowAnswer] = useState("");
   const renderProblems = () =>
     problems.map(problem => (
       <li
@@ -15,6 +18,7 @@ function App() {
         <div className="d-flex justify-content-between align-items-center w-100">
           <div>
             <span>{`Problem # ${problems.indexOf(problem) + 1}:  `}</span>
+            <span className="ml-1">{problem.title}</span>
             <a
               href={`https://projecteuler.net/problem=${problems.indexOf(
                 problem
@@ -22,7 +26,9 @@ function App() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <span>{problem.title}</span>
+              <i className="material-icons pl-2" style={{ fontSize: "16px" }}>
+                open_in_new
+              </i>
             </a>
           </div>
           <Button
@@ -37,17 +43,34 @@ function App() {
           <SyntaxHighlighter language="javascript" style={okaidia}>
             {problem.code}
           </SyntaxHighlighter>
-          {/* <pre>
-            <code>{problem.code}</code>
-          </pre> */}
+          <div className="d-flex align-items-center justify-content-between">
+            <Button color="link" onClick={() => toggleProblem(problem.title)}>
+              {showAnswer ? "Hide Answer" : "Show Answer"}
+            </Button>
+            <span
+              style={{
+                display: showAnswer === problem.title ? "block" : "none"
+              }}
+            >
+              <Answer fn={problem.fn} />
+            </span>
+          </div>
         </Collapse>
       </li>
     ));
   const toggleIsOpen = title => {
+    setShowAnswer("");
     if (isOpen === title) {
       setIsOpen("");
     } else {
       setIsOpen(title);
+    }
+  };
+  const toggleProblem = title => {
+    if (showAnswer === title) {
+      setShowAnswer("");
+    } else {
+      setShowAnswer(title);
     }
   };
   return (
@@ -57,6 +80,7 @@ function App() {
     >
       <h1 className="display-4 mb-4 text-muted">Euler 100</h1>
       <ul className="list-group w-100">{renderProblems()}</ul>
+      <Footer />
     </div>
   );
 }
