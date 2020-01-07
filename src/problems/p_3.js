b7bd44c4d1bbef2858b5d476bb068a624f753191
Kimfucious <kimfucious@abts.io>
2020-01-07 23:04:13 +0700

    simplify code format

--- src/problems/p_3.js
+++ src/problems/p_3.js
@@ -1,18 +1,5 @@
+export const id = 3;
 export const title = "Largest prime factor";
-export const code = `const fn = n => {
-  let x = n;
-  let i = 2;
-  while (i < x) {
-    while (x % i === 0) {
-      (y => {
-        x = y;
-      })(x / i);
-    }
-    i++;
-  }
-  return x;
-};
-`;
 export const fn = (n = 600851475143) => {
   let x = n;
   let i = 2;
@@ -26,3 +13,5 @@ export const fn = (n = 600851475143) => {
   }
   return x;
 };
+
+export const code = fn.toString();
