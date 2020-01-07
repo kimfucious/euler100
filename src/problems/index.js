2676a90946fba11ff3ce155b9a70f887af5b0dd2
Kimfucious <kimfucious@abts.io>
2020-01-07 23:02:14 +0700

    add problems index

--- src/problems/index.js
+++ src/problems/index.js
@@ -1,21 +1,8 @@
 import * as p1 from "./p_1";
 import * as p2 from "./p_2";
 import * as p3 from "./p_3";
+import * as p4 from "./p_4";
+import * as p5 from "./p_5";
+import * as p6 from "./p_6";
 
-export const problems = [
-  {
-    title: p1.title,
-    code: p1.code,
-    fn: p1.fn
-  },
-  {
-    title: p2.title,
-    code: p2.code,
-    fn: p2.fn
-  },
-  {
-    title: p3.title,
-    code: p3.code,
-    fn: p3.fn
-  }
-];
+export const problems = [p1, p2, p3, p4, p5, p6];
