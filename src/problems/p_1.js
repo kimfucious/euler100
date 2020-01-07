03c91cad4a7bf7852aa5af065bc94adfdc252416
Kimfucious <kimfucious@abts.io>
2020-01-07 23:02:48 +0700

    simplify code format

--- src/problems/p_1.js
+++ src/problems/p_1.js
@@ -1,5 +1,7 @@
+export const id = 1;
 export const title = "Multiples of 3 or 5";
-export const code = `const fn = () => {
+export const fn = () => {
+  try {
     let sum = 0;
     for (let i = 0; i < 1000; i++) {
       if (i % 3 === 0 || i % 5 === 0) {
@@ -7,14 +9,9 @@ export const code = `const fn = () => {
       }
     }
     return sum;
-  };
-`;
-export const fn = () => {
-  let sum = 0;
-  for (let i = 0; i < 1000; i++) {
-    if (i % 3 === 0 || i % 5 === 0) {
-      sum += i;
-    }
+  } catch (error) {
+    throw error;
   }
-  return sum;
 };
+
+export const code = fn.toString();
