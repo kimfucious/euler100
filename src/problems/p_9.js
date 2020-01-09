4cffe4cbe73efda7d4d1a7480c87984e5dc00590
Kimfucious <kimfucious@abts.io>
2020-01-10 03:01:06 +0700

    add problem solution 9

---
+++ src/problems/p_9.js
@@ -0,0 +1,27 @@
+export const id = 9;
+export const title = "Special Pythagorean triplet";
+export const code = `(n = 1000) => {
+  for (let c = Math.floor(n / 3 + 1); c < n / 2; c++) {
+    let abSquared = c * c - n * n + 2 * n * c;
+    let abSquareRoot = Math.floor(Math.sqrt(abSquared));
+    if (abSquareRoot * abSquareRoot === abSquared) {
+      let b = (n - c + abSquareRoot) / 2;
+      let a = n - b - c;
+      return a * b * c;
+    }
+  }
+  return -1;
+}`;
+
+export const fn = (n = 1000) => {
+  for (let c = Math.floor(n / 3 + 1); c < n / 2; c++) {
+    let abSquared = c * c - n * n + 2 * n * c;
+    let abSquareRoot = Math.floor(Math.sqrt(abSquared));
+    if (abSquareRoot * abSquareRoot === abSquared) {
+      let b = (n - c + abSquareRoot) / 2;
+      let a = n - b - c;
+      return a * b * c;
+    }
+  }
+  return -1;
+};
