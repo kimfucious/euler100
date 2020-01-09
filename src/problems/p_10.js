0176c1bb564fe6f43a09a23cae8c21110763ba5e
Kimfucious <kimfucious@abts.io>
2020-01-10 03:01:18 +0700

    add problem solution 10

---
+++ src/problems/p_10.js
@@ -0,0 +1,43 @@
+export const id = 10;
+export const title = "Summation of primes";
+export const code = `(n = 2000000) => {
+  let data = new Uint8Array(n + 1);
+  data[0] = 1;
+  data[1] = 1;
+  for (let i = 2; i <= Math.floor(Math.sqrt(n)); i++) {
+    if (data[i] === 0) {
+      for (let j = i + i; j <= n; j += i) {
+        if (data[j] === 0) {
+          data[j] = 1;
+        }
+      }
+    }
+  }
+  return data.reduce((acc, curr, idx) => {
+    if (curr === 0) {
+      acc += idx;
+    }
+    return acc;
+  }, 0);
+}`;
+
+export const fn = (n = 2000000) => {
+  let data = new Uint8Array(n + 1);
+  data[0] = 1;
+  data[1] = 1;
+  for (let i = 2; i <= Math.floor(Math.sqrt(n)); i++) {
+    if (data[i] === 0) {
+      for (let j = i + i; j <= n; j += i) {
+        if (data[j] === 0) {
+          data[j] = 1;
+        }
+      }
+    }
+  }
+  return data.reduce((acc, curr, idx) => {
+    if (curr === 0) {
+      acc += idx;
+    }
+    return acc;
+  }, 0);
+};
