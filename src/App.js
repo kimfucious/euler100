8e422558a0978b6ba2dbc2d5dd9e2d3636df5369
Kimfucious <kimfucious@abts.io>
2020-01-12 14:21:02 +0700

    get answers to be non-blocking add animations

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Collapse } from "reactstrap";
 import { problems } from "./problems";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { okaidia } from "react-syntax-highlighter/dist/esm/styles/prism";
 import { Footer } from "./components/Footer";
-import { Answer } from "./components/Answer";
 import { Pagination } from "./components/Pagination";
+import { getRandomAnimation } from "./helpers";
+import numeral from "numeral";
 
 function App() {
+  const [answer, setAnswer] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const [currentProblems, setCurrentProblems] = useState(problems);
+  const [currentProblem, setCurrentProblem] = useState("");
+  const [currentProblems, setCurrentProblems] = useState(problems.slice(0, 10));
   const [isOpen, setIsOpen] = useState("");
   const [showAnswer, setShowAnswer] = useState("");
   const setProblems = page => {
-    setCurrentProblems(problems.slice(page * 10 - 10, page * 10 + 1));
+    setCurrentProblems(problems.slice(page * 10 - 10, page * 10));
   };
   const handleBack = () => {
     if (currentPage > 1) {
@@ -39,7 +42,7 @@ function App() {
       >
         <div className="d-flex justify-content-between align-items-center w-100">
           <div>
-            <span>{`Problem # ${problems.indexOf(problem) + 1}:  `}</span>
+            <span>{`# ${problems.indexOf(problem) + 1}:  `}</span>
             <span className="ml-1">{problem.title}</span>
             <a
               href={`https://projecteuler.net/problem=${problems.indexOf(
@@ -66,21 +69,28 @@ function App() {
             {problem.code}
           </SyntaxHighlighter>
           <div className="d-flex align-items-center justify-content-between">
-            <Button color="link" onClick={() => toggleProblem(problem.title)}>
+            <Button
+              color="link"
+              size="sm"
+              onClick={() => toggleProblem(problem)}
+            >
               {showAnswer ? "Hide Answer" : "Show Answer"}
             </Button>
-            <span
-              style={{
-                display: showAnswer === problem.title ? "block" : "none"
-              }}
-            >
-              <Answer fn={problem.fn} />
-            </span>
+            {answer && answer === "Wait for it..." ? (
+              <span className="animated fadeIn">{answer}</span>
+            ) : null}
+            {answer && answer !== "Wait for it..." ? (
+              <span className={`animated ${getRandomAnimation()}`}>
+                {numeral(answer).format()}
+              </span>
+            ) : null}
           </div>
         </Collapse>
       </li>
     ));
   const toggleIsOpen = title => {
+    setAnswer("");
+    setCurrentProblem("");
     setShowAnswer("");
     if (isOpen === title) {
       setIsOpen("");
@@ -88,19 +98,44 @@ function App() {
       setIsOpen(title);
     }
   };
-  const toggleProblem = title => {
-    if (showAnswer === title) {
+  const toggleProblem = problem => {
+    if (!currentProblem) {
+      setCurrentProblem(problem);
+    } else {
+      setCurrentProblem("");
+    }
+    if (showAnswer === problem.title) {
+      setAnswer("");
       setShowAnswer("");
     } else {
-      setShowAnswer(title);
+      setShowAnswer(problem.title);
+      setAnswer("Wait for it...");
+      // const data = await problem.fn();
+      // setAnswer(data);
     }
   };
+  useEffect(() => {
+    const getAnswer = async fn => {
+      const data = await fn();
+      setAnswer(data);
+    };
+    if (currentProblem.fn && answer === "Wait for it...") {
+      getAnswer(currentProblem.fn);
+    }
+  }, [answer, currentProblem.fn]);
   return (
     <div
       className="container d-flex flex-column align-items-center mt-5"
       style={{ maxWidth: "800px" }}
     >
       <h1 className="display-4 mb-4 text-muted">{`Euler ${problems.length}`}</h1>
+      <Pagination
+        currentPage={currentPage}
+        handleBack={handleBack}
+        handleFwd={handleFwd}
+        handleGoToPage={handleGoToPage}
+        problemsLength={problems.length}
+      />
       <ul className="list-group w-100">{renderProblems()}</ul>
       <Pagination
         currentPage={currentPage}
