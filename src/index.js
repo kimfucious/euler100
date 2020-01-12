b709290249191cba8062cf5c85581f25ade6931f
Kimfucious <kimfucious@abts.io>
2020-01-12 14:17:14 +0700

    add animate.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/bootstrap-slate.css";
+import "animate.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
