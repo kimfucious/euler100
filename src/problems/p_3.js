feff5c074fa9a7033e9a3b7e8342c474193d5409
Kimfucious <kimfucious@abts.io>
2020-01-14 15:43:07 +0700

    update initial solution

--- src/problems/p_3.js
+++ src/problems/p_3.js
@@ -1,32 +1,84 @@
+import * as smart from "./smarties/p_3_smart";
 export const id = 3;
 export const title = "Largest prime factor";
+export const alt = smart.code;
+export const url = smart.url;
+export const altFn = smart.fn;
 export const code = `(n = 600851475143) => {
-  let x = n;
-  let i = 2;
-  while (i < x) {
-    while (x % i === 0) {
-      (y => {
-        x = y;
-      })(x / i);
+  const factorize = num => {
+    let factorMap = {};
+    let n = num;
+    let i = 2;
+
+    const count = n => {
+      if (factorMap[n]) ++factorMap[n];
+      else factorMap[n] = 1;
+    };
+
+    while (i * i <= n) {
+      while (n % i === 0) {
+        n /= i;
+        count(i);
+      }
+      i++;
     }
-    i++;
+
+    if (n !== num) {
+      if (n > 1) count(n);
+    } else {
+      count(num);
+    }
+    return factorMap;
+  };
+
+  let factorMap = factorize(n);
+  let max = 0;
+  for (let i in factorMap) {
+    max = Math.max(max, i);
   }
-  return x;
-}`;
+  return max;
+};
+`;
+
 export const fn = (n = 600851475143) => {
   return new Promise((resolve, reject) => {
-    try {
-      let x = n;
-      let i = 2;
-      while (i < x) {
-        while (x % i === 0) {
-          (y => {
-            x = y;
-          })(x / i);
+    const factorize = num => {
+      try {
+        let factorMap = {};
+        let n = num;
+        let i = 2;
+
+        const count = n => {
+          if (factorMap[n]) ++factorMap[n];
+          else factorMap[n] = 1;
+        };
+
+        while (i * i <= n) {
+          while (n % i === 0) {
+            n /= i;
+            count(i);
+          }
+          i++;
         }
-        i++;
+
+        if (n !== num) {
+          if (n > 1) count(n);
+        } else {
+          count(num);
+        }
+        return factorMap;
+      } catch (error) {
+        reject(error);
       }
-      resolve(x);
+    };
+
+    try {
+      let factorMap = factorize(n);
+      let max = 0;
+      for (let i in factorMap) {
+        max = Math.max(max, i);
+      }
+      resolve(max);
     } catch (error) {
       reject(error);
     }
